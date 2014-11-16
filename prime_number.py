d903646ba295c7b3d8b89950e2114dd80c3b3e10
fluent93 <fluent93@gmail.com>
2014-11-16 12:39:48 +0900

    no message

--- prime_number.py
+++ prime_number.py
@@ -1,10 +1,11 @@
 __author__ = 'changhan.ryu'
 
+import math
+
 def isPrime(n):
     if n < 2:
         return False
-    else:
-        for i in range(2, n):
-            if n % i == 0:
-                return False
+    for i in range(2, int(math.sqrt(n))+1):
+        if n % i == 0:
+            return False
     return True
