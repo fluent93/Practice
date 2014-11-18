cc4358c7725e02c580d67593addd02aa32254e14
changhan.ryu <fluent93@gmail.com>
2014-11-18 16:38:47 +0900

    sum of self numbers VERSION 2: sum of whole numbers - sum of non-self numbers

--- self_number.py
+++ self_number.py
@@ -1,21 +1,14 @@
 __author__ = 'changhanryu'
 
 
-def is_selfNumber(n):
-    origin_number = 0
-    digit_sum = 0
-    total_sum = 0
-    while origin_number < n:
-        s = str(origin_number)
-        for i in s:
-            digit_sum += int(i)
-        total_sum = origin_number + digit_sum
-        if total_sum == n:
-            return False
-        origin_number += 1
-        digit_sum = 0
-    return True
+def SN_sum():
+    S = sum(range(5000))
+    GN = sum([digit_sum(i) for i in range(5000) if digit_sum(i) < 5000])
+    return S - GN
 
-def sum_of_selfNumber(n):
-    lst = [x for x in range(n) if is_selfNumber(x)]
-    return sum(lst)
+def digit_sum(n):
+    s = str(n)
+    total = 0
+    for i in s:
+        total += int(i)
+    return total + n
