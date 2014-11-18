6ceb59c9cd745aa78be028852bf2beddea5fccd8
changhan.ryu <fluent93@gmail.com>
2014-11-18 17:01:42 +0900

    VERSION 4: sum of whole numbers - sum of non self numbers
    It's the same result of the sum(set of whole numbers) - set of non self numbers) --> VERSION 3

--- self_number.py
+++ self_number.py
@@ -2,9 +2,9 @@ __author__ = 'changhanryu'
 
 
 def SN_sum():
-    S = set(range(5000))
-    GN = set([digit_sum(i) for i in range(5000) if digit_sum(i) < 5000])
-    return sum(S - GN)
+    S = sum(range(5000))
+    GN = sum(set([digit_sum(i) for i in range(5000) if digit_sum(i) < 5000]))
+    return S - GN
 
 def digit_sum(n):
     s = str(n)
