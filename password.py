03f50759fb892cc01049ccd9b0af25df079e3715
changhan.ryu <fluent93@gmail.com>
2014-12-22 14:03:39 +0900

    Merge branch 'master' of https://github.com/fluent93/Practice

--- password.py
+++ password.py
@@ -1,1 +1,21 @@
 __author__ = 'changhan.ryu'
+
+def secure_pwd(str) :
+    count_digit = 0
+    count_lower = 0
+    count_upper = 0
+
+    if len(str) < 10:
+        return False
+    else:
+        for i in str:
+            if i.isdigit():
+                count_digit += 1
+            if i.islower():
+                count_lower += 1
+            if i.isupper():
+                count_upper += 1
+
+        if count_upper >=1 and count_lower >=1 and count_digit >=1:
+            return True
+        return False
